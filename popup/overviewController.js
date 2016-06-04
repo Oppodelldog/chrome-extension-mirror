85ea288862e35e85effcb401aa2635177a7f3509
odog <odog@netcologne.de>
2016-06-04 20:00:45 +0200

    added little angular app as blueprint for configuration popup

--- popup/overviewController.js
+++ popup/overviewController.js
@@ -1,35 +1,35 @@
-/*
-	First scratch version to test capabilities and access to data
+angular.module('app').controller("OverviewController", function($scope){
+    var vm = this;
+    vm.title = 'Mirror 	Extension';
 
-	this script displays the tabs that are controlled by the current tab.
-*/
-window.onload = function() {
+    loadMirroredTabs(function(mirroredTabs){
+    	vm.mirroredTabs = mirroredTabs;
+    	$scope.$apply();
+    });
+});
 
+function loadMirroredTabs(successFunc){
+	
 	var tabCouples = chrome.extension.getBackgroundPage().tabCouples;
-	content = document.getElementById("content");
 	
 	chrome.tabs.getSelected(null,function(currentTab){
 		chrome.tabs.query({}, function(allTabs) {
-			var tabHTML = "";
 			var tabInfo = getTabInfoFromTabs(allTabs);
 			
 			for(tabId in tabCouples){
 				if(tabId != currentTab.id){
 					continue;
 				}
-				tabHTML += "this tab mirrors to the following tabs:<br><ul>";
+				mirroredTabs = [];
 				for(e in tabCouples[tabId])	{
 					var coupledTabId = tabCouples[tabId][e];
-					tabHTML += "<li>" + tabInfo[coupledTabId].title + "</li>";
+					mirroredTabs.push(tabInfo[coupledTabId]);
 				}
-				tabHTML +="</ul>";
 			}
-
-		content.innerHTML = tabHTML;
+		successFunc(mirroredTabs)
 		});
 	});
-
-};
+}
 
 function getTabInfoFromTabs(tabs){
 	var tabInfo = [];
