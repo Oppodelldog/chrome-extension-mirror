736a8694821bc49b0e85959f230219d2cd3e885e
Oppodelldog <odog@netcologne.de>
2020-10-18 14:17:01 +0200

    cleanup

--- content/ui.js
+++ content/ui.js
@@ -1,11 +1,10 @@
 function handleCouplingStatusChange(request) {
-    console.log(request.value, request.groupName)
     let elem = document.createElement('div');
     elem.id = "mirror-extension-couplingIndicator"
     const css = 'position:fixed;box-sizing: border-box;top:0;right:0;width:40px;height:40px;opacity:0.3;z-index:100;background-color:#00ff00;';
     const existingElem = document.getElementById(elem.id);
 
-    if (typeof existingElem !== "undefined" && existingElem !== null) {
+    if (existingElem !== null) {
         elem = existingElem
     } else {
         document.body.appendChild(elem);
