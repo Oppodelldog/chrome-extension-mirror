08716e6b45e19c0afc091e5d4dea545e94759574
Oppodelldog <odog@netcologne.de>
2020-10-18 21:55:07 +0200

    remove broken overview which was anyway in the wrong place. information regarding coupling of a tab shall be in the new UI element in the content.

--- popup/router.js
+++ popup/router.js
@@ -2,11 +2,6 @@
 angular.module('app').config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/overview', {
-        templateUrl: 'templates/overview.html',
-        controller: 'OverviewController',
-        controllerAs: 'vm',
-      }).
       when('/configuration', {
         templateUrl: 'templates/configuration.html',
         controller: 'ConfigurationController',
